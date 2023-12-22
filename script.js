151c2c7e08d9d4d87687608e16fb5f21039648cd
svparekh <setulvp@gmail.com>
2023-12-21 22:10:47 -0600

    new styles

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function toggleMenu() {
     const menu = document.querySelector(".mini-nav-menu-items");
     const icon = document.querySelector(".mini-nav-menu-icon");
+    const menu_bg = document.querySelector(".mini-nav-menu-bg");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+    menu_bg.classList.toggle("open");
 }
